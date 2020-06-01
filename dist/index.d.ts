9fd17b9e7992f61fd512b5230fde05dd6b34e33c
Master Chief <16513382+117@users.noreply.github.com>
2020-06-01 12:25:11 -0700

    make sure Stream class is exported

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export { Client } from './lib/client';
+export { Stream } from './lib/stream';
 export { URL } from './lib/common';
 export { Account, Order, Position, Asset, Watchlist, Calendar, Clock, AccountConfigurations, TradeActivity, NonTradeActivity, PortfolioHistory, Bars, Bar, LastTradeResponse, LastQuoteResponse, } from './lib/entities';
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export { Client } from './lib/client'
+export { Stream } from './lib/stream'
 export { URL } from './lib/common'
 
 export {
