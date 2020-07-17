85ddc3bacfdb956997aeee60b26a62ff7adc5a1f
117 <117@users.noreply.github.com>
2020-07-17 03:46:51 +0000

    made prettier 🦄

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var client_1 = require("./lib/client");
-Object.defineProperty(exports, "Client", { enumerable: true, get: function () { return client_1.Client; } });
+Object.defineProperty(exports, "Client", {
+  enumerable: true,
+  get: function () {
+    return client_1.Client;
+  },
+});
 var stream_1 = require("./lib/stream");
-Object.defineProperty(exports, "Stream", { enumerable: true, get: function () { return stream_1.Stream; } });
+Object.defineProperty(exports, "Stream", {
+  enumerable: true,
+  get: function () {
+    return stream_1.Stream;
+  },
+});
 var url_1 = require("./lib/url");
-Object.defineProperty(exports, "URL", { enumerable: true, get: function () { return url_1.URL; } });
+Object.defineProperty(exports, "URL", {
+  enumerable: true,
+  get: function () {
+    return url_1.URL;
+  },
+});
