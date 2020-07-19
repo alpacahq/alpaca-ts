fdf7962c15310520ea534fd328cf32fdc43d38e1
Master Chief <16513382+117@users.noreply.github.com>
2020-07-19 10:22:58 -0700

    local dist

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var client_1 = require("./lib/client");
-Object.defineProperty(exports, "Client", {
-  enumerable: true,
-  get: function () {
-    return client_1.Client;
-  },
-});
+Object.defineProperty(exports, "Client", { enumerable: true, get: function () { return client_1.Client; } });
 var stream_1 = require("./lib/stream");
-Object.defineProperty(exports, "Stream", {
-  enumerable: true,
-  get: function () {
-    return stream_1.Stream;
-  },
-});
+Object.defineProperty(exports, "Stream", { enumerable: true, get: function () { return stream_1.Stream; } });
