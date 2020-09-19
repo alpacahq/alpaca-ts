7a90a76674eadbe871f52ebabc994f4600edfcb5
117 <117@users.noreply.github.com>
2020-09-19 00:52:19 +0000

    made prettier 🦄

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var client_1 = require("./lib/client");
-Object.defineProperty(exports, "AlpacaClient", { enumerable: true, get: function () { return client_1.AlpacaClient; } });
+Object.defineProperty(exports, "AlpacaClient", {
+  enumerable: true,
+  get: function () {
+    return client_1.AlpacaClient;
+  },
+});
 var stream_1 = require("./lib/stream");
-Object.defineProperty(exports, "AlpacaStream", { enumerable: true, get: function () { return stream_1.AlpacaStream; } });
+Object.defineProperty(exports, "AlpacaStream", {
+  enumerable: true,
+  get: function () {
+    return stream_1.AlpacaStream;
+  },
+});
