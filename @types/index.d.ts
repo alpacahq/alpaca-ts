4cdf9359891d6a8eaafb565df7256af89a0bdb09
Master Chief <16513382+117@users.noreply.github.com>
2021-02-04 18:38:36 -0700

    Remove extension

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,4 +1,4 @@
-export { AlpacaClient } from './client.js';
-export { AlpacaStream } from './stream.js';
+export { AlpacaClient } from './client';
+export { AlpacaStream } from './stream';
 export { Account, Order, Position, Asset, Watchlist, Calendar, Clock, DefaultCredentials, OAuthCredentials, AccountConfigurations, NonTradeActivity, TradeActivity, PortfolioHistory, Bar, LastQuote, LastTrade, } from './entities';
 export { GetOrder, GetOrders, PlaceOrder, ReplaceOrder, CancelOrder, GetPosition, ClosePosition, GetAsset, GetAssets, GetWatchList, CreateWatchList, UpdateWatchList, AddToWatchList, RemoveFromWatchList, DeleteWatchList, GetCalendar, UpdateAccountConfigurations, GetAccountActivities, GetPortfolioHistory, GetBars, GetLastTrade, GetLastQuote, } from './params';
