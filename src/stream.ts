cb3413e5e058ca2b0ae11a1e9fb7f0bef255316a
tclare <tclare@nd.edu>
2022-02-22 15:53:34 -0500

    stream.ts: getConnection added

--- src/stream.ts
+++ src/stream.ts
@@ -159,6 +159,16 @@ export class AlpacaStream extends EventEmitter<string | symbol | any> {
   }
 
   /**
+   * Retrieve the underlying WebSocket connection AlpacaStream uses.
+   * Now callers can read and modify properties of the web socket 
+   * i.e., close the websocket with AlpacaStream.getConnection().close().
+   * @returns the of type WebSocket
+   */
+  getConnection() {
+    return this.connection;
+  }
+
+  /**
    * Subscribe to an account or data stream channel.
    * @param channel trades, quotes, bars, trade_updates
    * @param symbols only use with data stream ex. [ "AAPL", "TSLA", ... ]
