028298ae5df535ab0b7d143b1c189d0b2d3979d9
Nico G <16513382+117@users.noreply.github.com>
2022-04-06 17:17:24 -0600

    Fix url #100

--- src/urls.ts
+++ src/urls.ts
@@ -2,7 +2,7 @@ import { DataSource } from './entities';
 
 export default {
   rest: {
-    beta: 'https://api.alpaca.markets/v1beta1',
+    beta: 'https://data.alpaca.markets/v1beta1',
     account: 'https://api.alpaca.markets/v2',
     market_data_v2: 'https://data.alpaca.markets/v2',
     market_data_v1: 'https://data.alpaca.markets/v1',
