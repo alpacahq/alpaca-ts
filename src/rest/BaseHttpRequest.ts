bfd8c4b91baa2cfaf2ea307783ba7ceca4f67dd0
alessiocastrica <alessio.castrica@alpaca.markets>
2023-08-02 19:27:43 +0200

    feat: smaller fixes and added BaseURL enum

--- src/rest/BaseHttpRequest.ts
+++ src/rest/BaseHttpRequest.ts
@@ -1,9 +1,11 @@
 import type { ApiRequestOptions } from "./ApiRequestOptions";
 import type { CancelablePromise } from "./CancelablePromise";
-import type { OpenAPIConfig } from "../OpenAPI";
+import type { OpenAPIConfig } from "../Client";
 
 export abstract class BaseHttpRequest {
-  constructor(public readonly config: OpenAPIConfig) {}
+  constructor(public readonly config: OpenAPIConfig) {
+    this.config = config;
+  }
 
   public abstract request<T>(options: ApiRequestOptions): CancelablePromise<T>;
 }
